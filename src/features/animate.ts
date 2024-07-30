a8a18bec36f0e33a2dffb7c65c179cb72b073714
Marti Serra <martiserramolina99@gmail.com>
2024-07-30 17:43:19 +0200

    Created form component

---
+++ src/features/animate.ts
@@ -0,0 +1,20 @@
+import { createContext, Dispatch, SetStateAction, useContext } from "react";
+
+export type Animate = "none" | "next" | "back";
+
+export interface AnimateValue {
+  animate: Animate;
+  setAnimate: Dispatch<SetStateAction<Animate>>;
+}
+
+export const AnimateContext = createContext<AnimateValue | null>(null);
+
+export function useAnimate(): AnimateValue {
+  const context = useContext(AnimateContext);
+  if (!context) {
+    throw new Error(
+      "useAnimate must be used within an AnimateContext.Provider",
+    );
+  }
+  return context;
+}
