24187fe1ea98b7c022a432518e2844f6c22c3a18
Marti Serra <martiserramolina99@gmail.com>
2024-07-31 09:57:25 +0200

    Removed logo

--- src/components/app/form/index.tsx
+++ src/components/app/form/index.tsx
@@ -1,7 +1,5 @@
 import { Formity, OnReturn } from "formity";
 
-import Logo from "../../logo";
-
 import components from "./components";
 import schema from "./schema";
 
@@ -12,7 +10,6 @@ interface FormProps {
 export default function Form({ onReturn }: FormProps) {
   return (
     <div className="relative h-screen">
-      <Logo className="absolute left-4 top-4" />
       <Formity components={components} schema={schema} onReturn={onReturn} />
       <div className="absolute bottom-4 right-4 w-max select-none rounded-full border border-white/10 bg-white/5 px-6 py-2.5 text-sm text-white/50">
         Powered by Formity
