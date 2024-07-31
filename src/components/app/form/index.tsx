791fe79b8ba36c3c723f479d85dd1a21b32917d5
Marti Serra <martiserramolina99@gmail.com>
2024-07-31 09:58:28 +0200

    Removed powered by formity

--- src/components/app/form/index.tsx
+++ src/components/app/form/index.tsx
@@ -11,9 +11,6 @@ export default function Form({ onReturn }: FormProps) {
   return (
     <div className="relative h-screen">
       <Formity components={components} schema={schema} onReturn={onReturn} />
-      <div className="absolute bottom-4 right-4 w-max select-none rounded-full border border-white/10 bg-white/5 px-6 py-2.5 text-sm text-white/50">
-        Powered by Formity
-      </div>
     </div>
   );
 }
